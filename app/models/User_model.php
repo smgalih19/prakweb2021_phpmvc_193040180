892f023ebf682638343e455264b427514ad7df85
Sugih Mohammad Galih <59138555+smgalih19@users.noreply.github.com>
2021-10-01 20:11:49 +0700

    Membuat Aplikasi MVC dengan PHP video 7 Selesai

---
+++ app/models/User_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class User_model{
+  private $nama = 'Sugih';
+
+  public function getUser(){
+    return $this->nama;
+  }
+}
+
+?>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,4 @@
 <div class="container">
     <h1 class="display-4">Selamat Datang di Website Saya</h1>
+    <p class="lead">Halo, nama saya <?= $data['nama']; ?></p>
 </div>
